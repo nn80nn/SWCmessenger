a4c33b7047579806bbb69f0ec394e03c6fbf589e
nn80nn <nn80nn80nn80nn80nn@gmail.com>
2025-02-16 14:17:18 +0300

    add Spring boot and Maven

---
+++ src/main/java/nvi/server/swcmessenger/SwCmessengerApplication.java
@@ -0,0 +1,13 @@
+package nvi.server.swcmessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwCmessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwCmessengerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nvi/server/swcmessenger/SwCmessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package nvi.server.swcmessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwCmessengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
